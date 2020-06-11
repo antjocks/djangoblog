ae053258c7ff453b9f494f14f8ab371764bfd1ec
antjocks <antjocks@gmail.com>
2020-06-10 23:35:53 -0700

    Added post save functionality

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
